d7023dc375953a0e23112caa762517382848772e
Tomer.Gabay <tomer.gabay@woonstadrotterdam.nl>
2023-03-22 13:54:04 +0100

    added ModuleNotFound part

--- src/streamlit_random_generator/main_page.py
+++ src/streamlit_random_generator/main_page.py
@@ -1,3 +1,30 @@
+# based on https://discuss.streamlit.io/t/pip-installing-from-github/21484/5
+import subprocess
+import sys
+import time
+import streamlit as st
+
+try:
+    import streamlit_random_generator
+
+# This block executes only on the first run when your package isn't installed
+except ModuleNotFoundError as e:
+    sleep_time = 30
+    dependency_warning = st.warning(
+        f"Installing dependencies, this takes {sleep_time} seconds."
+    )
+    subprocess.Popen(
+        [
+            f"{sys.executable} -m pip install git+https://${{github_token}}@github.com/sTomerG/streamlit_random_generator.git",
+        ],
+        shell=True,
+    )
+
+    # wait for subprocess to install package before running your actual code below
+    time.sleep(sleep_time)
+    # remove the installing dependency warning
+    dependency_warning.empty()
+
 import streamlit as st
 from streamlit_random_generator.functions import random_int
 
